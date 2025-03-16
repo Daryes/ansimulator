9ba7fa51f82acaef223c34f2bee7923b7a6fd58d
Daryes <ogme+github@proton.me>
2025-03-16 15:00:29 +0100

    ci docker: DinD updated

--- .ci/docker-ansimulator-template/DinD/refresh_dind_hack.sh
+++ .ci/docker-ansimulator-template/DinD/refresh_dind_hack.sh
@@ -3,16 +3,22 @@
 #
 # informations: 
 # https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
-# https://hub.docker.com/_/docker/
+# https://hub.docker.com/_/docker/  => DinD
 
 
-# source: https://github.com/docker/docker/blob/master/hack/dind / alt : https://github.com/moby/moby/blob/master/hack/dind
+# source: https://github.com/docker/docker/tree/master/hack/dind
+# alt :   https://github.com/moby/moby/blob/master/hack/dind
+DIND_COMMIT=a2a6bf51fae014453a8f60058270a3a052e0e529
+DIND_URL="https://raw.githubusercontent.com/docker/docker/${DIND_COMMIT}/hack/dind"
 
-DIND_COMMIT=d58df1fc6c866447ce2cd129af10e5b507705624/hack/dind
+if command -v wget; then
+    wget -O dind "${DIND_URL}"
+    [ $? -ne 0 ] && exit 1
 
-wget -O dind "https://raw.githubusercontent.com/docker/docker/${DIND_COMMIT}/hack/dind"
-[ $? -ne 0 ] && exit 1
-
-chmod +x dind
+else
+    curl "${DIND_URL}" --output dind
+    [ $? -ne 0 ] && exit 1
+fi
 
 echo "Done"
+
