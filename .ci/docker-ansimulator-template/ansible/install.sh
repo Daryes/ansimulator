b40c2f26d5dde369a9b4c7e33d6eb241b1df40eb
Daryes <ogme+github@proton.me>
2025-07-08 12:03:18 +0200

    ci docker: dockerfile + ansible updated to use a single installation script + ansible requirements moved to a dedicated file + support for ansible v2.19 installation

--- .ci/docker-ansimulator-template/ansible/install.sh
+++ .ci/docker-ansimulator-template/ansible/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -eux
+set -eu
 
 # change to current script dir
 cd $( dirname $0 )
 # -----------------------------------------------------------------------------
-# The variables *_VERSION and ANSIBLE_USER_UID come from the docker build argument 
+# The variables *_VERSION and ANSIBLE_USER_UID can come from the docker build argument 
+ANSIBLE_VERSION=${ANSIBLE_VERSION:-}
+ANSIBLE_USER_UID=${ANSIBLE_USER_UID:-421}
+
 ANSIBLE_HOME_DIR=/home/ansible
 
 ANSIBLE_PACKAGE_NAME=ansible-core
@@ -15,9 +18,93 @@ ANSIBLE_PIP_EXTRA_ARGS=""
 ANSIBLE_PIP_DEPS_CRYPTO_VERSION="<41"
 ANSIBLE_PIP_DEPS_PSYCOPG_PACKAGE="psycopg2-binary"
 
-if [ "$BASE_SYSTEM" == "debian" ]; then
-  ANSIBLE_PIP_EXTRA_ARGS="--break-system-packages"
-fi
+
+# optional components
+INSTALL_ANSIBLE_LINT=0
+INSTALL_PLAYBOOK_GRAPHER=0
+INSTALL_ANSIBLE_NAVIGATOR=0
+
+
+ANSIBLE_LINT_VERSION=${ANSIBLE_LINT_VERSION:-}
+ANSIBLE_PLAYBOOK_GRAPHER_VERSION=${ANSIBLE_PLAYBOOK_GRAPHER_VERSION:-}
+ANSIBLE_NAVIGATOR_VERSION=${ANSIBLE_NAVIGATOR_VERSION:-}
+
+
+THIS_SCRIPT_DIR="$( dirname $0 )"
+# functions ####################################################################
+
+# integrated help
+_showUsage() {
+    echo -e "Syntax : $( basename $0 ) --base-system redhat|debian  --ansible-version 'x.y'
+                                       [--user-uid ${ANSIBLE_USER_UID}]
+                                       [--install-ansible-lint]
+                                       [--install-ansible-navigator]
+                                       [--install-playbook-grapher]
+                                       [--debug]
+"
+}
+
+
+# main #########################################################################
+
+# command line parameters
+if [ $# -eq 0 ]; then _showUsage; exit 1; fi
+
+while [ $# -gt 0 ]; do
+    arg="$1"
+    # support for both -param and --param syntax
+    if echo "$arg" | egrep -q '^--[a-z]+'; then arg="${arg:1}"; fi
+
+    case $arg in
+        -help)
+            _showUsage
+            exit 0
+            ;;
+
+        -debug)
+            set -eux
+            ;;
+
+        -base-system)
+            BASE_SYSTEM=$2
+            shift
+            ;;
+
+        -ansible-version)
+            ANSIBLE_VERSION=$2
+            shift
+            ;;
+
+        -user-uid)
+            ANSIBLE_USER_UID=$2
+            shift
+            ;;
+
+        -install-ansible-lint)
+            INSTALL_ANSIBLE_LINT=1
+            ;;
+
+        -install-ansible-navigator)
+            INSTALL_ANSIBLE_NAVIGATOR=1
+            ;;
+
+        -install-playbook-grapher)
+            INSTALL_PLAYBOOK_GRAPHER=1
+            ;;
+
+
+        # inconnu
+        *)
+            echo "Unknown argument : $arg"
+            _showUsage
+            exit 1
+            ;;
+
+        esac
+
+    #  argument suivant
+    [ $# -gt 0 ] && shift
+done
 
 
 case "$ANSIBLE_VERSION" in
@@ -25,32 +112,38 @@ case "$ANSIBLE_VERSION" in
   2.9.*)
     ANSIBLE_PACKAGE_NAME=ansible
     ANSIBLE_PIP_DEPS_CRYPTO_VERSION='==3.*'
-    ANSIBLE_LINT_VERSION="4.3.*  rich<11.0.0"
     ANSIBLE_PIP_DEPS_PSYCOPG_PACKAGE="psycopg2"
     ANSIBLE_PIP_EXTRA_ARGS=""
+    ANSIBLE_LINT_VERSION="4.3.*  rich<11.0.0"
+    ANSIBLE_PLAYBOOK_GRAPHER_VERSION="1.0.0.dev4"
     ;;
 
   2.1[45].*)
-    ANSIBLE_LINT_VERSION="6.*"
     ANSIBLE_PIP_EXTRA_ARGS=""
+    ANSIBLE_LINT_VERSION="6.*"
+    ANSIBLE_PLAYBOOK_GRAPHER_VERSION="2.0.0"
     ;;
 
-  2.16.*)
-    # no release available yet for Rocky with python3 >= 3.10
+  2.1[6789].*)
+    # no release available yet for Rocky with native python3 > 3.9
     if [ "$BASE_SYSTEM" == "redhat" ]; then 
-      if grep -q -i --fixed-strings 'VERSION_ID="9.' /etc/os-release; then
-        ANSIBLE_PYTHON_PACKAGES_RPM="python3.11 python3.11-pip python3.11-devel"
-        shopt -s expand_aliases
-        alias python3=/usr/bin/python3.11
-      fi
+        if grep -q -i --fixed-strings 'VERSION_ID="9.' /etc/os-release; then
+            ANSIBLE_PYTHON_PACKAGES_RPM="python3.11 python3.11-pip python3.11-devel"
+            shopt -s expand_aliases
+            alias python3=/usr/bin/python3.11
+        fi
     fi
     ;;
 
 esac
 
-if [ ! -z "$ANSIBLE_LINT_VERSION" ]; then
-    ANSIBLE_LINT_VERSION="==$ANSIBLE_LINT_VERSION";
+
+# TODO: use a venv instead
+if [ "$BASE_SYSTEM" == "debian" ]; then
+  ANSIBLE_PIP_EXTRA_ARGS="${ANSIBLE_PIP_EXTRA_ARGS} --break-system-packages"
 fi
+
+
 
 # ansible pip requirements
 # => netaddr : ip netmask calculation
@@ -62,9 +155,7 @@ fi
 # 
 ANSIBLE_PIP_DEPS="httplib2 six cryptography$ANSIBLE_PIP_DEPS_CRYPTO_VERSION netaddr jmespath PyMySQL[rsa] $ANSIBLE_PIP_DEPS_PSYCOPG_PACKAGE yamllint"
 
-
-# ansible collections to install
-ANSIBLE_COLLECTIONS="ansible.netcommon ansible.utils ansible.posix ansible.windows community.general community.crypto community.mysql community.postgresql"
+# The ansible collections are loaded from the requirements.yml file
 
 
 # ansible user creation and sudo rights
@@ -112,13 +203,9 @@ python3 -m pip install --upgrade --no-cache $ANSIBLE_PIP_EXTRA_ARGS pip wheel
 python3 -m pip install --no-cache ${ANSIBLE_PIP_EXTRA_ARGS}  $ANSIBLE_PIP_DEPS
 python3 -m pip install --no-cache ${ANSIBLE_PIP_EXTRA_ARGS} "$ANSIBLE_PACKAGE_NAME==$ANSIBLE_VERSION"
 
-python3 -m pip install --no-cache ${ANSIBLE_PIP_EXTRA_ARGS} "ansible-lint${ANSIBLE_LINT_VERSION}"
 
-
-# ansible: collections installation
-for TARGET_COLLECTION in $ANSIBLE_COLLECTIONS; do
-	su - ansible -c "ansible-galaxy collection install $TARGET_COLLECTION"
-done
+# ansible: collections installation - provided by the requirements file
+su - ansible -c "ansible-galaxy collection install -r ${THIS_SCRIPT_DIR}/requirements.yml"
 
 
 # Fix for the docker environment and authorized key - must be last in this script
@@ -126,9 +213,9 @@ done
 cat >> $ANSIBLE_HOME_DIR/.bashrc  << 'EOT'
 export USER=$( whoami )
 
-if [ ! -z "$ANSIBLE_VAULT_PASS_FILE" ] && [ ! -s "$ANSIBLE_VAULT_PASS_FILE" ]; then
-  sudo touch "$ANSIBLE_VAULT_PASS_FILE" && sudo chown ansible: "$ANSIBLE_VAULT_PASS_FILE" && chmod 600 "$ANSIBLE_VAULT_PASS_FILE"
-  echo "VAULT PASSKEY CHANGE ME" > "$ANSIBLE_VAULT_PASS_FILE"
+if [ ! -z "$ANSIBLE_VAULT_PASSWORD_FILE" ] && [ ! -s "$ANSIBLE_VAULT_PASSWORD_FILE" ]; then
+  sudo install --owner ansible --group ansible --mode 600 -T /dev/null "$ANSIBLE_VAULT_PASSWORD_FILE"
+  echo "VAULT PASSKEY CHANGE ME" > "$ANSIBLE_VAULT_PASSWORD_FILE"
 fi
 
 if [ ! -d ~/.ssh ]; then
@@ -144,6 +231,51 @@ if [ ! -f ~/.ssh/authorized_keys ]; then cp -p  ~/.ssh/id_rsa.pub  ~/.ssh/author
 
 cd /etc/ansible/
 EOT
+
+
+# ansible-lint ################################################################
+
+if [ $INSTALL_ANSIBLE_LINT -eq 1 ]; then
+    echo ""
+    echo "Installing ansible-lint ..."
+
+    if [ ! -z "$ANSIBLE_LINT_VERSION" ]; then ANSIBLE_LINT_VERSION="==$ANSIBLE_LINT_VERSION"; fi
+
+    export PIP_ROOT_USER_ACTION=ignore
+    python3 -m pip install --no-cache ${ANSIBLE_PIP_EXTRA_ARGS} "ansible-lint${ANSIBLE_LINT_VERSION}"
+fi
+
+
+# ansible-navigator ###########################################################
+
+if [ $INSTALL_ANSIBLE_NAVIGATOR -eq 1 ]; then
+    echo ""
+    echo "Installing ansible-navigator ..."
+
+    if [ ! -z "$ANSIBLE_NAVIGATOR_VERSION" ]; then ANSIBLE_NAVIGATOR_VERSION="==$ANSIBLE_NAVIGATOR_VERSION"; fi
+
+    export PIP_ROOT_USER_ACTION=ignore
+    python3 -m pip install --no-cache ${ANSIBLE_PIP_EXTRA_ARGS} "ansible-navigator[ansible-core]${ANSIBLE_NAVIGATOR_VERSION}"
+fi
+
+
+# playbook-grapher ###########################################################
+
+if [ $INSTALL_PLAYBOOK_GRAPHER -eq 1 ]; then
+    echo ""
+    echo "Installing playbook-grapher ..."
+
+    if [ ! -z "$ANSIBLE_PLAYBOOK_GRAPHER_VERSION" ]; then ANSIBLE_PLAYBOOK_GRAPHER_VERSION="==$ANSIBLE_PLAYBOOK_GRAPHER_VERSION"; fi
+
+    # requirement
+    bash wrapper-packages_install.sh \
+        graphviz
+
+    # ansible: python requirements and installation
+    export PIP_ROOT_USER_ACTION=ignore
+    python3 -m pip install --upgrade --no-cache ${ANSIBLE_PIP_EXTRA_ARGS} ansible-playbook-grapher$ANSIBLE_PLAYBOOK_GRAPHER_VERSION
+fi
+
 
 # cleanup
 bash wrapper-packages_clean.sh
